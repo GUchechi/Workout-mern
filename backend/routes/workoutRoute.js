7a89d96db1044e8cda8006256e47cf864f78732a
GUchechi <godswilluchendu7@gmail.com>
2023-09-20 12:04:17 +0100

    added updateWorkout and deleteWorkout

--- backend/routes/workoutRoute.js
+++ backend/routes/workoutRoute.js
@@ -3,6 +3,8 @@ const {
   createWorkout,
   getWorkouts,
   getWorkout,
+  updateWorkout,
+  deleteWorkout,
 } = require("../controllers/workoutController");
 
 const router = express.Router();
@@ -17,13 +19,9 @@ router.get("/:id", getWorkout);
 router.post("/", createWorkout);
 
 // UPDATE a workout
-router.patch("/:id", (req, res) => {
-  res.json({ mssg: "UPDATE a workout" });
-});
+router.patch("/:id", updateWorkout);
 
 // DELETE a workout
-router.delete("/:id", (req, res) => {
-  res.json({ mssg: "DELETE a workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 module.exports = router;
