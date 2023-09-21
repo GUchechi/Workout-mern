54c21a970d48bb6b0150e32d31034e8b5b08165d
GUchechi <godswilluchendu7@gmail.com>
2023-09-21 01:30:45 +0100

    added Delete workout

--- frontend/src/components/WorkoutDetails.jsx
+++ frontend/src/components/WorkoutDetails.jsx
@@ -1,20 +1,40 @@
 import { format } from "date-fns";
+import { useWorkoutsContext } from "../hooks/useWorkoutContext";
 
 const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
   // Parse the ISO date string into a Date object
   const createdAtDate = new Date(workout.createdAt);
   const formattedDate = format(createdAtDate, "MMM dd, yyyy hh:mmaa");
 
- 
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: data });
+    }
+  };
 
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
-      <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Number of reps: </strong>{workout.reps}</p>
+      <p>
+        <strong>Load (kg): </strong>
+        {workout.load}
+      </p>
+      <p>
+        <strong>Number of reps: </strong>
+        {workout.reps}
+      </p>
       <p>{formattedDate}</p>
+      <span onClick={handleClick}>Delete</span>
     </div>
-  )
-}
+  );
+};
 
-export default WorkoutDetails
+export default WorkoutDetails;
