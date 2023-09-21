8f1bd0074ab204ba189978546953ac333a7cd840
GUchechi <godswilluchendu7@gmail.com>
2023-09-21 13:34:08 +0100

    added userLogin

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,27 @@
 const asyncHandler = require("express-async-handler");
 const mongoose = require("mongoose");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
-// login user
-const loginUser = asyncHandler(async (req, res) => {
-  res.json({ mssg: "login user" });
-});
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "1d" });
+};
+
+// login a user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 // signup user
 const signupUser = asyncHandler(async (req, res) => {
@@ -13,7 +29,11 @@ const signupUser = asyncHandler(async (req, res) => {
 
   try {
     const user = await User.signup(username, email, password);
-    res.status(200).json({ username, email, user });
+
+    // create a token
+    const token = createToken(user._id);
+
+    res.status(200).json({ username, email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -57,4 +57,24 @@ userSchema.statics.signup = async function (username, email, password) {
   return user;
 };
 
+// Static login method
+// static login method
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Incorrect login credentials");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect login credentials");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
